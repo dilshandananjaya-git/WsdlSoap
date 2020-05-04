19d504fac32dc9800a1da21c772af2c0fef9b8f0
Dilshan Dananjaya <Dilshan.D@CreativeSoftware.com>
2020-05-04 17:46:05 +0530

    initial commit

---
+++ CalWebApplication/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CalWebApplication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnaddd_Click(object sender, EventArgs e)
+        {
+            calServiceReference.CalculatorWebServiceSoapClient client = new calServiceReference.CalculatorWebServiceSoapClient();
+            int result = client.add(Convert.ToInt32(txtFirstNumber.Text), Convert.ToInt32(txtSecondNumber.Text));
+            lblResult.Text = result.ToString();
+        }
+    }
+}
